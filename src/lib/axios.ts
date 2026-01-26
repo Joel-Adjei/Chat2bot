4d0eddae6fab9d90117ff6d6b8860659c737c78d
Joel-Adjei <kofienergy15@gmail.com>
2026-01-26 07:27:13 +0000

    Key update

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_OPENAPI_BASEURL;
-const apiKey = import.meta.env.VITE_API;
 
 const useAxios = () =>
   axios.create({
     baseURL: baseURL,
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
+      Authorization: `Bearer ${import.meta.env.VITE_API}`,
     },
   });
 
